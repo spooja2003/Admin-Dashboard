189cb6e1c73bc507c2bf5023ffe344fe208c579c
spooja2003 <139827475+spooja2003@users.noreply.github.com>
2025-06-23 21:16:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App";
-// import './index.css';
-
-// //import { ThemeProviderCustom } from "./theme/useThemeContext";
-// import { ThemeContextProvider } from './theme/useThemeContext'; // ✅
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <ThemeContextProvider>
-//       <App />
-//     </ThemeContextProvider>
-
-//   </React.StrictMode>
-// );
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
